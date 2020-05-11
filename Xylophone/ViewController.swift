df2958c96f4d89917105a7d9871e0abca6cbb19d
Naohisa Yokoyama <nhsykym@gmail.com>
2020-05-11 23:48:45 +0900

    Implement audio playback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let resourceName: String = sender.currentTitle!
+        playSound(resourceName: resourceName)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
     
-
+    
+    func playSound(resourceName: String) {
+        let url = Bundle.main.url(forResource: resourceName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
